cc6340ae68a615b1cbcdeb105c13b12253332115
AntonRosenberg <79712669+AntonRosenberg@users.noreply.github.com>
2023-12-03 16:07:49 +0100

    Day 1 solved

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,19 +1,53 @@
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <cstring>
 #include <vector>
 
 std::size_t find_number_sum(std::string& string)
 {
+    std::string sum = "";
+    std::string firstNum;
+    std::string lastNum;
     std::vector<std::string> textNums = {"one","two","three","four","five","six","seven","eight","nine"};
-    std::string sum;
-    const char firstNum = string[string.find_first_of("0123456789")];
-    sum.push_back(firstNum);
-    std::reverse(string.begin(), string.end());
-    const char lastNum = string[string.find_first_of("0123456789")];
-    sum.push_back(lastNum);
+    auto minDist = (std::size_t)std::distance(string.begin(), string.end()); 
+    for (int i = 0; i<textNums.size(); i++)
+    {
+        auto elem = textNums[i];
+        auto indFirstLetNum = string.find(elem);
+        if (indFirstLetNum != string.npos && indFirstLetNum < minDist)
+        {
+            minDist = indFirstLetNum;
+            firstNum = std::to_string(i+1);
+        }    
+    }
+    std::size_t indFirstNumber = string.find_first_of("0123456789");
+    if (indFirstNumber < minDist)
+    {
+        firstNum = string[indFirstNumber];
+    }
+    minDist = (std::size_t)std::distance(string.begin(), string.end()); 
+    sum.append(firstNum);
 
+    std::reverse(string.begin(), string.end());
+    for (int i = 0; i<textNums.size(); i++)
+    {
+        auto elem = textNums[i];
+        std::reverse(elem.begin(), elem.end());
+        auto indFirstLetNum = string.find(elem);
+        if (indFirstLetNum != string.npos && indFirstLetNum < minDist)
+        {
+            minDist = indFirstLetNum;
+            lastNum = std::to_string(i+1);
+        }    
+    }
+    std::size_t indLastNum = string.find_first_of("0123456789");
+    if (indLastNum < minDist)
+    {
+        lastNum = string[indLastNum];
+    }
+    sum.append(lastNum);
     return std::stoi(sum);
 }
 
